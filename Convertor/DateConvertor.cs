5510166dc40e46c6ff2b4dc931a1148c6b9a6263
DeV1 <danialshokouhmanesh@gmail.com>
2021-03-22 16:34:59 +0430

    Shamsi date Convertor added

---
+++ Convertor/DateConvertor.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Globalization;
+namespace StatisticsApp.Convertor
+{
+    public static class DateConvertor
+    {
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetYear(value) + "/" + pc.GetMonth(value).ToString("00") + pc.GetDayOfMonth(value).ToString("00");
+        }
+    }
+}
